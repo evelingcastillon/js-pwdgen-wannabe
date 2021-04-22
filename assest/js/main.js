9d96c6a46edcf0d8897cf71edc2808c50b49e7d3
evelingcastillon <evelingcastillon0304@gmail.com>
2021-04-22 16:49:29 +0200

    password esplicita

--- assest/js/main.js
+++ assest/js/main.js
@@ -6,3 +6,6 @@ var userSurname = prompt("Qual è il tuo cognome?");
 
 //Chiedi colore preferito
 var favColor = prompt("Qual'è il tuo colore preferito?");
+
+//Scrivere password in modo esplicito
+document.getElementById('password-user').innerHTML = userName + userSurname + favColor + 21
